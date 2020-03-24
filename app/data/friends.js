0d947d6034f73f8b9843b1a7665eb013ff113c4f
Nate Foster <soundwubz@gmail.com>
2020-03-24 08:01:04 -0400

    apiRoutes set up. form validation added. placeholder friends added.

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+let friends = [
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },    
+    {
+        "name":"Amy",
+        "photo":"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fcringe&psig=AOvVaw2mHnFnOY5zo7fQL674GHqp&ust=1585135431616000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKDM_-7_sugCFQAAAAAdAAAAABAT",
+        "scores":[
+            3,
+            2,
+            6,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }  
+];
+
+module.exports = friends;
